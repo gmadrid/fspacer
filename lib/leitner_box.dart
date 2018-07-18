9b19579f74c489cf0758d1652d6900680bc7bc9a
George Madrid <gmadrid@gmail.com>
2018-07-18 07:57:18 -0400

    LeitnerBox works with tests.

--- lib/leitner_box.dart
+++ lib/leitner_box.dart
@@ -2,21 +2,45 @@ import 'question.dart';
 
 class LeitnerBox {
   LeitnerBox() {
-//    _buckets = List();
-//    for (var i = 0; i < _total_buckets; ++i) {
-//      _buckets.add(List());
-//    }
+    _buckets = List();
+    for (var i = 0; i < _total_buckets; ++i) {
+      _buckets.add(List());
+    }
   }
 
-  void AddQuestions(Iterable<Question> questions) {}
-  num Size() {return 0;}
-  num BucketSize(num index) { return 0; }
+  void AddQuestions(Iterable<Question> questions) {
+    _buckets[0].addAll(questions);
+    _num_questions += questions.length;
+  }
+  num Size() {return _num_questions;}
+  num BucketSize(num index) { return _buckets[index].length; }
 
-  Question Next(num index) { return null; }
+  Question Next(num index) {
+    return _buckets[index].last;
+  }
 
   void Shuffle(num index) { return; }
-  void MoveToFirst(num index) { return; }
-  void MoveUp(num index) { return; }
+
+  void MoveToFirst(num index) {
+    if (BucketSize(index) < 1) return;
+
+    var from_bucket = _buckets[index];
+    var q = from_bucket.last;
+    from_bucket.removeLast();
+    _buckets[first_bucket].insert(0, q);
+  }
+
+  void MoveUp(num index) {
+    if (BucketSize(index) < 1) return;
+
+    // TODO: have a way to deal with this case.
+    if (index == last_bucket) return;
+
+    var from_bucket = _buckets[index];
+    var q = from_bucket.last;
+    from_bucket.removeLast();
+    _buckets[index + 1].insert(0, q);
+  }
 
   static const num first_bucket = 1;
   static const num last_bucket = 7;
@@ -25,4 +49,5 @@ class LeitnerBox {
   static const num _total_buckets = last_bucket + 1;
 
   List<List<Question>> _buckets;
+  num _num_questions = 0;
 }
--- test/leitner_box_test.dart
+++ test/leitner_box_test.dart
@@ -37,4 +37,60 @@ void main() {
     lb.MoveToFirst(0);
     lb.MoveUp(0);
   });
+
+  test("starts in bucket 0", () {
+    var size = 34;
+    var lb = SimpleTestSet(size);
+    expect(lb.Size(), equals(size));
+    expect(lb.BucketSize(0), equals(size));
+  });
+
+  test("simple next", () {
+    var size = 3;
+    var lb = SimpleTestSet(size);
+
+    var q = lb.Next(0);
+    expect(q.q, equals(Q(size - 1)));
+  });
+
+  test("simple moves", () {
+    var size = 5;
+    var lb = SimpleTestSet(size);
+    expect(lb.BucketSize(0), equals(size));
+
+    var q0 = lb.Next(0);
+    lb.MoveToFirst(0);
+    expect(lb.BucketSize(0), equals(size - 1));
+    expect(lb.BucketSize(1), equals(1));
+
+    var q1 = lb.Next(0);
+    expect(q1, isNot(equals(q0)));
+
+    var q2 = lb.Next(1);
+    expect(q2, equals(q0));
+  });
+
+  test("deeper moves", () {
+    var size = 11;
+    var lb = SimpleTestSet(size);
+    expect(lb.BucketSize(0), equals(size));
+
+    lb.MoveToFirst(0);
+    lb.MoveToFirst(0);
+    lb.MoveToFirst(0);
+    lb.MoveToFirst(0);
+    lb.MoveToFirst(0);
+    lb.MoveToFirst(0);
+
+    lb.MoveUp(1);
+    lb.MoveUp(1);
+    lb.MoveUp(1);
+
+    lb.MoveUp(2);
+
+    expect(lb.BucketSize(0), equals(5));
+    expect(lb.BucketSize(1), equals(3));
+    expect(lb.BucketSize(2), equals(2));
+    expect(lb.BucketSize(3), equals(1));
+  });
 }
